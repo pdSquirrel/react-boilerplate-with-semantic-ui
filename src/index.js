a366552c5bfea450ad1168e6b2c62c2399c770a1
Roger Norqvist <progdev.squirrel@gmail.com>
2017-07-04 14:02:00 +0200

    added material-ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 const title = 'React Boilerplate with Semantic-UI';
 
+class ExampleComponent extends Component {
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <RaisedButton primary={true} label="Logga in" />
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <div>{title}</div>,
+  <ExampleComponent />,
   document.getElementById('app')
 );
 
